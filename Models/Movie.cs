3b1541e11ec3518539945ad931180ab6d16c17af
Nick D'Oria <17dorin@gmail.com>
2021-02-19 12:33:59 -0500

    Finished Lab

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public DateTime year { get; set; }
+        public string actors { get; set; }
+        public string directors { get; set; }
+    }
+}
